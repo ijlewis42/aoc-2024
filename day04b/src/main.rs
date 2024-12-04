b32e97a9bdd798f60a61d6e6e8a328c06d32d196
Ian Lewis <ian@giantmargarita.com>
2024-12-04 23:10:25 +1100

    Day 4b: cleaned up a little, and added comments

--- day04b/src/main.rs
+++ day04b/src/main.rs
@@ -4,55 +4,62 @@ fn main() {
     let stdin = io::stdin();
     let lines = stdin.lines();
 
-    let mut total = 0;
+    let mut grid : Vec<Vec<char>> = Vec::new();
 
-    let mut grid : Vec<String> = Vec::new();
     // non-idiomatic approach to reading all the input
     for line in lines {
         let line = line.unwrap();
-        //let values = line.split_whitespace().map(|x| x.parse::<i32>().unwrap()).collect::<Vec<_>>();
-        //println!("{:?}", values);
 
-        // pair each adjacent value
-        //let adjacents = values.windows(2);
-        //println!("{:?}", adjacents.clone().collect::<Vec<_>>());
-
-        //total += 0;
-
-        grid.push(line);
+        // add each line as a vector of chars
+        grid.push(line.chars().collect());
     }
 
-    let word = "MAS";
+    // word as a vector of chars
+    let word:Vec<char> = "MAS".chars().collect();
 
-    for y in 0..grid.len() {
-        for x in 0..grid[0].len() {
+    // size of grid
+    let height = grid.len() as i32;
+    let width = grid[0].len() as i32;
+
+    let mut total = 0;
+
+    // loop over the whole grid
+    for y in 0..height {
+        for x in 0..width {
             let mut subtotal = 0;
-            for dx in [-1i32, 1] {
+            // look in four diagonal directions, but treat (x,y) as the CENTRE point, not the top-left point
+            for dx in [-1i32, 1] {              // still so gross looking, and seems more silly with so few options now
                 for dy in [-1i32, 1] {
-                    if dx == 0 && dy == 0 { continue };
-
+                    // assume success
                     let mut success = true;
-                    for i in 0..word.len() {
-                        let px: i32 = x as i32 + dx * (i as i32 - 1);
-                        let py: i32 = y as i32 + dy * (i as i32 - 1);
 
-                        if px < 0 || py < 0 || px >= (grid[0].len() as i32) || py >= (grid.len() as i32) {
+                    // loop over all the characters in the word
+                    for i in 0..word.len() as i32 {
+                        // work out coordinates from (x, y) position, with an offset in current direction, with a distance equal to current character of word
+                        let px: i32 = x + dx * (i - 1);     // offset the index by -1 to treat (x,y) as CENTRE point
+                        let py: i32 = y + dy * (i - 1);
+
+                        // not success if out of bounds
+                        if px < 0 || py < 0 || px >= width || py >= height {
                             success = false;
                             break;
                         }
 
-                        if grid[py as usize].chars().nth(px as usize).unwrap() != word.chars().nth(i).unwrap() {
+                        // not success if character in grid doesn't match character in the word
+                        if grid[py as usize][px as usize] != word[i as usize] {
                             success = false;
                             break;
                         }
                     } 
 
+                    // count this found copy of the word
                     if success {
                         subtotal += 1;
                     }
                 }
             }  
 
+            // if found two copies with same centre, we have a X-"MAS"
             if subtotal == 2 {
                 total += 1;
             }
