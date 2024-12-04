f407cb3a8eade60341e1afee9af30486f4ad2cce
Ian Lewis <ian@giantmargarita.com>
2024-12-04 22:34:29 +1100

    Day 4a: cleaned up a little, and added comments

--- day04a/src/main.rs
+++ day04a/src/main.rs
@@ -4,48 +4,54 @@ fn main() {
     let stdin = io::stdin();
     let lines = stdin.lines();
 
-    let mut total = 0;
+    let mut grid : Vec<Vec<char>> = Vec::new();
 
-    let mut grid : Vec<String> = Vec::new();
     // non-idiomatic approach to reading all the input
     for line in lines {
         let line = line.unwrap();
-        //let values = line.split_whitespace().map(|x| x.parse::<i32>().unwrap()).collect::<Vec<_>>();
-        //println!("{:?}", values);
 
-        // pair each adjacent value
-        //let adjacents = values.windows(2);
-        //println!("{:?}", adjacents.clone().collect::<Vec<_>>());
-
-        //total += 0;
-
-        grid.push(line);
+        // add each line as a vector of chars
+        grid.push(line.chars().collect());
     }
 
-    let word = "XMAS";
+    let word:Vec<char> = "XMAS".chars().collect();
+    let height = grid.len() as i32;
+    let width = grid[0].len() as i32;
 
-    for y in 0..grid.len() {
-        for x in 0..grid[0].len() {
-            for dx in [-1i32, 0, 1] {
+    let mut total = 0;
+
+    // loop over the whole grid
+    for y in 0..height {
+        for x in 0..width {
+            // look in all 8 directions
+            for dx in [-1i32, 0, 1] {       // so gross looking
                 for dy in [-1i32, 0, 1] {
+                    // skip middle case (both offsets are zero)
                     if dx == 0 && dy == 0 { continue };
 
+                    // assume success
                     let mut success = true;
-                    for i in 0..word.len() {
-                        let px: i32 = x as i32 + dx * (i as i32);
-                        let py: i32 = y as i32 + dy * (i as i32);
 
-                        if px < 0 || py < 0 || px >= (grid[0].len() as i32) || py >= (grid.len() as i32) {
+                    // loop over all the characters in the word
+                    for i in 0..word.len() as i32 {
+                        // work out coordinates from (x, y) position, with an offset in current direction, with a distance equal to current character of word
+                        let px: i32 = x + dx * i;
+                        let py: i32 = y + dy * i;
+
+                        // not success if out of bounds
+                        if px < 0 || py < 0 || px >= width || py >= height {
                             success = false;
                             break;
                         }
 
-                        if grid[py as usize].chars().nth(px as usize).unwrap() != word.chars().nth(i).unwrap() {
+                        // not success if character in grid doesn't match character in the word
+                        if grid[py as usize][px as usize] != word[i as usize] {
                             success = false;
                             break;
                         }
                     } 
 
+                    // count this found copy of the word
                     if success {
                         total += 1;
                     }
