4f93a0472109ebd36172370f827840ee913875d5
Ian Lewis <ian@giantmargarita.com>
2024-12-06 10:09:04 +1100

    Day 5a: Somewhat better structured code, and added comments.

--- day05a/src/main.rs
+++ day05a/src/main.rs
@@ -4,67 +4,56 @@ use std::collections::HashMap;
 
 fn main() {
     let stdin = io::stdin();
-    let lines = stdin.lines();
-
-    let mut total = 0;
-
-    let mut firstSection = true;
+    let mut lines = stdin.lines();
 
     let mut before: HashMap<i32, Vec<i32>> = HashMap::new();
 
     // non-idiomatic approach to reading all the input
-    for line in lines {
-        let line = line.unwrap();
+    // process the top section of the input until we reach an empty line
+    while let Some(Ok(line)) = lines.next() {
         //println!("{:?}", line);
 
-        if line.len() == 0 
-        {
-            firstSection = false;
-            continue;
-        }
+        // empty line found, bail out
+        if line.len() == 0 { break };
 
-        if firstSection {
-            let parts: Vec<i32> = line.split("|").map(|x| x.parse::<i32>().unwrap()).collect();
+        let pages: Vec<i32> = line.split("|").map(|x| x.parse::<i32>().unwrap()).collect();
+        //println!("{} {}", pages[0], pages[1]);
+        
+        before
+            .entry(pages[0]) // find the vector of pages after this page
+            .or_insert(Vec::new())  // or make a new vector 
+            .push(pages[1]);                              // add the new page
+    }
 
-            println!("{} {}", parts[0], parts[1]);
-            before.entry(parts[0]).or_insert(Vec::new()).push(parts[1]);
-            /*if before.contains_key(&parts[0]) {
-                before.get(&parts[0]).unwrap().push(parts[1]);
-            } else {
-                before.insert(parts[0], Vec::new());
-            }*/
-        } else {
-            let parts: Vec<i32> = line.split(",").map(|x| x.parse::<i32>().unwrap()).collect();
+    let mut total = 0;
 
-            let mut success = true;
-            for i in 0..parts.len()-1 {
-                for j in i+1..parts.len() {
-                    match before.get(&parts[i]) {
-                        Some(list) => {
-                            if !list.contains(&parts[j]) {
-                                success = false;
-                                println!("NOT {} before {}", parts[i], parts[j]);
-                            } else {
-                                println!("{} before {}", parts[i], parts[j]);
-                            }
-                        },
-                        None => {
-                            success = false;
-                            println!("NO MATCH");
-                        }
-                    } 
+    // process the bottom section of the input, and calculate the total as we go
+    while let Some(Ok(line)) = lines.next() {
+        let pages: Vec<i32> = line.split(",").map(|x| x.parse::<i32>().unwrap()).collect();
+        
+        let mut is_correctly_ordered = true;
+
+        // loop through all pages in the list (except the last as it is correctly ordered if we get that far)
+        for i in 0..pages.len()-1 {
+            match before.get(&pages[i]) {
+                // if current page has a entry in the before list, then check that all following pages in the list are present in it
+                Some(list) => {
+                    if !pages.iter().skip(i + 1).all(|item| list.contains(item)) {
+                        is_correctly_ordered = false;
+                        break;
+                    }
+                },
+                // if current page isn't in the before list, can't have a correct ordering
+                None => {
+                    is_correctly_ordered = false;
+                    break;
                 }
             }
-
-            if success {
-                total += parts[parts.len() / 2];
-                println!("woo!");
-            } else {
-                println!("boo!");
-            }
         }
 
-
+        if is_correctly_ordered {
+            total += pages[pages.len() / 2];
+        }
     }
 
     println!("{}", total);
