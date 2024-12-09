bffd04f0eb3863a311a1307063d14e87aacebad4
Ian Lewis <ian@giantmargarita.com>
2024-12-09 15:44:10 +1100

    Day 8: Cleaned up both parts.

--- day08a/src/main.rs
+++ day08a/src/main.rs
@@ -7,20 +7,20 @@ fn main() {
     let lines = stdin.lines();
 
     let mut grid : Vec<Vec<char>> = Vec::new();
-
     let mut antennas: HashMap<char, Vec<(i32, i32)>> = HashMap::new();
     
     // non-idiomatic approach to reading all the input
     for (y, line) in lines.enumerate() {
         let line = line.unwrap();
-        
         let line = line.chars().collect::<Vec<char>>();
 
         // add each line as a vector of chars
         grid.push(line.clone());
 
         for (x, c) in line.into_iter().enumerate() {
+            // if an antenna at this location
             if c != '.' {
+                // add it to the list of antennas of this type
                 antennas
                 .entry(c)
                 .or_insert(Vec::new())
@@ -35,40 +35,45 @@ fn main() {
 
     let mut total = 0;
 
-    for (antenna_type, antennas) in antennas {
-        for i in 0..antennas.len() {
-            for j in i + 1..antennas.len() {
-                let (x1, y1) = antennas[i];
-                let (x2, y2) = antennas[j];
-
+    // loop through all the antenna types that were found
+    for (_antenna_type, antennas) in antennas {
+        // loop through each pair of antenna
+        for (i, (x1, y1)) in antennas.iter().enumerate() {
+            for (x2, y2) in antennas.iter().skip(i + 1) {
+                // direction to second antenna from the first
                 let (dx, dy) = (x2 - x1, y2 - y1);
 
+                // locations of antinodes
                 let (nx1, ny1) = (x1 - dx, y1 - dy);
                 let (nx2, ny2) = (x2 + dx, y2 + dy);
 
-                if nx1 >= 0 && nx1 < width && ny1 >= 0 && ny1 < height {
-                    if grid[ny1 as usize][nx1 as usize] != '#' {
-                        grid[ny1 as usize][nx1 as usize] = '#';
-                        total += 1;
+                // I suspect the ugliness of this means it was really time to make grid a class
+                // declaring the closure here, as if it was declared outside this loop, it would "still exist" (due to scope lifetimes) by the time grid needed to be mutated further down (see A and B)
+                let test_and_update = |x: i32, y: i32, grid: &mut Vec<Vec<char>>| {
+                    if x >= 0 && x < width && y >= 0 && y < height && grid[y as usize][x as usize] != '#' {
+                        grid[y as usize][x as usize] = '#';
+                        return true;
                     }
-                }
+                    return false;
+                };
 
-                if nx2 >= 0 && nx2 < width && ny2 >= 0 && ny2 < height {
-                    if grid[ny2 as usize][nx2 as usize] != '#' {
-                        grid[ny2 as usize][nx2 as usize] = '#';
-                        total += 1;
-                    }
+                // if a new spot for an antinode, add it and count it                
+                if test_and_update(nx1, ny1, &mut grid) {
+                    total += 1;
+                }
+                // if a new spot for an antinode, add it and count it                
+                if test_and_update(nx2, ny2, &mut grid) {
+                    total += 1;
                 }
             }
-
         }
     }
 
-    /*for line in grid.clone() {
+    /*// output the final grid
+    for line in grid.clone() {
         println!("{}", line.iter().collect::<String>())
     }
     println!("");*/
-
 
     println!("{}", total);
 }
--- day08b/src/main.rs
+++ day08b/src/main.rs
@@ -1,26 +1,26 @@
 use std::io;
 use std::collections::HashMap;
-//use itertools::Itertools;
+use std::cmp::max;
 
 fn main() {
     let stdin = io::stdin();
     let lines = stdin.lines();
 
     let mut grid : Vec<Vec<char>> = Vec::new();
-
     let mut antennas: HashMap<char, Vec<(i32, i32)>> = HashMap::new();
     
     // non-idiomatic approach to reading all the input
     for (y, line) in lines.enumerate() {
         let line = line.unwrap();
-        
         let line = line.chars().collect::<Vec<char>>();
 
         // add each line as a vector of chars
         grid.push(line.clone());
 
         for (x, c) in line.into_iter().enumerate() {
+            // if an antenna at this location
             if c != '.' {
+                // add it to the list of antennas of this type
                 antennas
                 .entry(c)
                 .or_insert(Vec::new())
@@ -35,26 +35,29 @@ fn main() {
 
     let mut total = 0;
 
+    // loop through all the antenna types that were found
     for (_antenna_type, antennas) in antennas {
-        for i in 0..antennas.len() {
-            for j in i + 1..antennas.len() {
-                let (x1, y1) = antennas[i];
-                let (x2, y2) = antennas[j];
-
+        // loop through each pair of antenna
+        for (i, (x1, y1)) in antennas.iter().enumerate() {
+            for (x2, y2) in antennas.iter().skip(i + 1) {
+                // direction to second antenna from the first
                 let (dx, dy) = (x2 - x1, y2 - y1);
 
-                // this should not just be a magic number lol
-                for k in -100..=100 {
+                // we could be going in any direction from the first antenna, so potential loop max is a little complicated
+                let max = max(max(max(*x1, *y1), width - x1), height - y1);
+
+                for k in -max..=max {
+                //for k in -min(x1, y1)..=min(width - x1, height - y1) {
                     let (nx, ny) = (x1 - k * dx, y1 - k * dy);
 
-                    let borrowed_grid =  grid.clone();
-
+                    // declaring the closure here, as if it was declared outside this loop, it would "still exist" (due to scope lifetimes) by the time grid needed to be mutated further down (see A and B)
                     let test = |x: i32, y: i32| {
-                        x >= 0 && x < width && y >= 0 && y < height && borrowed_grid[y as usize][x as usize] != '#'
+                        x >= 0 && x < width && y >= 0 && y < height && grid[y as usize][x as usize] != '#'
                     };
     
-                    if test(nx, ny) { 
-                        grid[ny as usize][nx as usize] = '#';
+                    // if a new spot for an antinode, add it and count it
+                    if test(nx, ny) {                       // (A) test "ends" here, so immutable reference is "freed"
+                        grid[ny as usize][nx as usize] = '#';    // (B) safe to use the mutable reference again
                         total += 1;
                     }    
                 }
@@ -62,11 +65,11 @@ fn main() {
         }
     }
 
+    /*// output the final grid
     for line in grid.clone() {
         println!("{}", line.iter().collect::<String>())
     }
-    println!("");
-
+    println!("");*/
 
     println!("{}", total);
 }
