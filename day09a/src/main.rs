4513e92cbdc43f795e84e84d3dd81706e10a4bce
Ian Lewis <Ian.Lewis@utas.edu.au>
2024-12-10 14:26:04 +1100

    Day 09a. Some cleanup, not finished.

--- day09a/src/main.rs
+++ day09a/src/main.rs
@@ -1,6 +1,4 @@
 use std::io;
-use std::collections::HashMap;
-use std::cmp::max;
 
 fn main() {
     let stdin = io::stdin();
@@ -12,38 +10,37 @@ fn main() {
     let mut disc = Vec::new();
     
     for (x, c) in line.into_iter().enumerate() {
-        if x % 2 == 0 {
-            for _i in 0..c.to_digit(10).unwrap() {
-                disc.push(Some(x / 2));
-            }
-        } else {
-            for _i in 0..c.to_digit(10).unwrap() {
-                disc.push(None);
-            }
+        for _i in 0..c.to_digit(10).unwrap() {
+            disc.push(if x % 2 == 0 {
+                Some(x / 2)
+            } else {
+                None
+            });
         }
     }
 
     loop {
         let hole_to_fill = disc.iter().position(|&item| item == None);
 
-        let hole_to_fill = match hole_to_fill {
-            None => break,
-            Some(index) => index
-        };
+        if let Some(hole_to_fill) = hole_to_fill {
+            //println!("{hole_to_fill}");
 
-        println!("{hole_to_fill}");
-
-        disc[hole_to_fill] = disc.pop().unwrap();
-
-        while disc[disc.len() - 1] == None {
-            disc.pop();
+            disc[hole_to_fill] = disc.pop().unwrap();
+    
+            while disc[disc.len() - 1] == None {
+                disc.pop();
+            }
+        } else {
+            break;
         }
     }
 
+    //println!("{:?}", disc);
 
-    println!("{:?}", disc);
-
-    let total = disc.iter().enumerate().map(|(i, x)| i * x.unwrap());
+    // calculate total
+    let total = disc.iter()
+        .enumerate()
+        .map(|(i, x)| i * x.unwrap());
     let total = total.sum::<usize>();
 
     println!("{}", total);
