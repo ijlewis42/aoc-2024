77c094dfda8406427285a3d8c2c4c535a91e8e9d
Ian Lewis <ian@giantmargarita.com>
2024-12-20 09:38:31 +1100

    Day 19 Part 1 cleaned up.

--- day19a/src/main.rs
+++ day19a/src/main.rs
@@ -1,77 +1,53 @@
 use std::io;
-use regex::Regex;
+
+// this part could be easily done with the part 2 solution, just by changing what we total up, but kept this first version anyways
 
 fn main() {
     let stdin = io::stdin();
     let lines = stdin.lines();
 
-    // unwrap all the lines
-    //let lines = lines.map(|line| line.unwrap() + " ");
+    // unwrap and collect all the lines
+    let lines = lines.map(|line| line.unwrap());
+    let lines = lines.collect::<Vec<_>>();
 
-    let mut towels = Vec::new();
-    let mut first = true;
+    // grab all the comma separated towel descriptions
+    let towels = lines[0].split(", ").collect::<Vec<_>>();
+
     let mut total = 0;
-    for (y, line) in lines.enumerate() {
-        let line = line.unwrap();
 
-        if line.len() == 0 { continue; }
+    // loop through all the remaining lines of the file (each containing a single design)
+    for line in lines.iter().skip(2) {
+        let mut tried = Vec::new();
+        let mut todo = Vec::new();
+        todo.push(0);
 
-        //println!("{:?}", line.clone());
-        // add each line as a vector of chars
+        // attempt to make all designs of each length, and keep track of how many ways it is possible
+        'main_loop:
+        while !todo.is_empty() {
+            let current_index = todo.pop().unwrap();
 
-        println!("{y}: {:?}", line.clone());
+            // if we've been here before, don't do the work again
+            if tried.contains(&current_index) { continue; }
+            tried.push(current_index);
 
-        if first {
-            first = false;
-            let temp = line.split(", ");
-            let temp = temp.map(|s| s.chars().collect::<Vec<char>>());
-            let temp = temp.collect::<Vec<_>>();
-            towels = temp.clone();
-            println!("{:?}", temp);
-        } else {
-            let line = line.chars().collect::<Vec<char>>();
+            // loop through all the towel designs
+            for towel in towels.clone().into_iter() {
+                let end_index = current_index + towel.len();
+                // see if this towel design matches for this spot in the desired pattern (and doesn't go off the edge of the pattern)
+                if towel == &line[current_index..std::cmp::min(end_index, line.len())] {
+                    let next_index = current_index + towel.len();
 
-            let mut overall_success = false;
-            let mut tried = Vec::new();
-            let mut todo = Vec::new();
-            todo.push(0);
-
-            while !todo.is_empty() {
-                let current_index = todo.pop().unwrap();
-
-                if tried.contains(&current_index) { continue; }
-
-                tried.push(current_index);
-
-                for towel in &towels {
-                    let mut success = true;
-                    for i in 0..towel.len() {
-                        if current_index + i >= line.len() || line[current_index + i] != towel[i] {
-                            success = false;
-                            break;
-                        }
-                    }
-
-                    if success {
-                        let next_index = current_index + towel.len();
-                        if next_index == line.len() {
-                            overall_success = true;
-                        } else {
-                            if !todo.contains(&next_index) {
-                                todo.push(current_index + towel.len());
-                            }
-                        }
+                    // if we've reached the end of the pattern, then it can be made, so count it, and exit
+                    if next_index == line.len() {
+                        total += 1;
+                        break 'main_loop;
+                    // otherwise, keep searching from here
+                    } else {
+                        todo.push(next_index);
                     }
                 }
             }
-
-            if overall_success {
-                println!("SUCCESS!");
-                total += 1;
-            } else {
-                println!("FAILURE");
-            }
-        }        
+        }
     }
 
     println!("TOTAL: {total}");
