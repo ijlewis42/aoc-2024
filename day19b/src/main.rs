4336e9dccb12e2df3fd56815ad99082a86f0b1c9
Ian Lewis <ian@giantmargarita.com>
2024-12-20 09:25:32 +1100

    Day 19b. Cleaned up and commented.

--- day19b/src/main.rs
+++ day19b/src/main.rs
@@ -1,76 +1,43 @@
 use std::io;
-//use regex::Regex;
-use std::collections::HashMap;
 
 fn main() {
     let stdin = io::stdin();
     let lines = stdin.lines();
 
-    // unwrap all the lines
-    //let lines = lines.map(|line| line.unwrap() + " ");
+    // unwrap and collect all the lines
+    let lines = lines.map(|line| line.unwrap());
+    let lines = lines.collect::<Vec<_>>();
 
-    let mut towels = Vec::new();
-    let mut first = true;
+    // grab all the comma separated towel descriptions
+    let towels = lines[0].split(", ").collect::<Vec<_>>();
+
     let mut total = 0;
-    for (y, line) in lines.enumerate() {
-        let line = line.unwrap();
 
-        if line.len() == 0 { continue; }
+    // loop through all the remaining lines of the file (each containing a single design)
+    for line in lines.iter().skip(2) {
+        // initialise our dynamic programming style array to have 1 way to make a zero length design, and 0 ways to make all the other lengths
+        let mut subtotals: Vec<u128> = Vec::new();
+        subtotals.push(1);
+        subtotals.extend((0..line.len()).map(|_|0));
 
-        //println!("{:?}", line.clone());
-        // add each line as a vector of chars
-
-        println!("{y}: {:?}", line.clone());
-
-        if first {
-            first = false;
-            let temp = line.split(", ");
-            let temp = temp.map(|s| s.chars().collect::<Vec<char>>());
-            let temp = temp.collect::<Vec<_>>();
-            towels = temp.clone();
-            towels.sort_by(|a, b| (*a).len().cmp(&b.len()));
-            println!("{:?}", towels);
-        } else {
-            let line = line.chars().collect::<Vec<char>>();
-
-            let mut overall_success = false;
-            let len = line.len();
-            let mut totals: Vec<u128> = Vec::new();
-            for _i in 0..len+1 {
-                totals.push(0);
-            }
-            totals[0] = 1;
-
-            for current_index in 0..line.len() {
-                if current_index > 0 && totals[current_index] == 0 {
-                    continue;
-                }
-
-                for towel in &towels {
-                    let mut success = true;
-                    for i in 0..towel.len() {
-                        if current_index + i >= line.len() || line[current_index + i] != towel[i] {
-                            success = false;
-                            break;
-                        }
-                    }
-
-                    if success {
-                        let next_index = current_index + towel.len();
-                        totals[next_index] += totals[current_index];
+        // attempt to make all designs of each length, and keep track of how many ways it is possible
+        for current_index in 0..line.len() {
+            // if no ways to make the current length, not going to be able to make longer lengths _from_ here, so skip it
+            if subtotals[current_index] > 0 {
+                // loop through all the towel designs
+                for towel in towels.clone().into_iter() {
+                    let end_index = current_index + towel.len();
+                    // see if this towel design matches for this spot in the desired pattern (and doesn't go off the edge of the pattern)
+                    if towel == &line[current_index..std::cmp::min(end_index, line.len())] {
+                        // if so, add the amount of ways we could make this pattern
+                        subtotals[end_index] += subtotals[current_index];
                     }
                 }
             }
+        }
 
-            overall_success = totals[totals.len() - 1] > 0;
-            if overall_success {
-                println!("{:?}", totals);
-                println!("SUCCESS! {}", totals[totals.len() - 1]);
-                total += totals[totals.len() - 1];
-            } else {
-                println!("FAILURE");
-            }
-        }        
+        // number of ways we can make this pattern is stored in the last position (if zero, i.e. a failure, add it on anyway)
+        total += subtotals[subtotals.len() - 1];
     }
 
     println!("TOTAL: {total}");
